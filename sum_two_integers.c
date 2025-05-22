141580addc49468878d129cd0729931f186d4ea0
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-22 21:44:09 +0200

    mod

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 int main()
-<<<<<<< HEAD
-{
-    int nb1, nb2;
-    scanf("%d %d", &nb1, &nb2);
-    printf("La somme est : %d\n", nb1 + nb2);
-}
-=======
-{.  //déclaration des variables 
+{   //déclaration des variables 
     int nb1, nb2, somme;
     // lire les valeurs qu'on va additionner
     scanf("%d %d", &nb1, &nb2);
     // affiche la somme de ces nombres
     printf("La somme est : %d\n", nb1 + nb2);
 }
->>>>>>> eac83f185f3fe739fc79681f3adaf22cdea97af7
+
