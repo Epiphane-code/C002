2b080120ee334f24e997460d4573a0ede6730f47
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-22 22:03:32 +0200

    ajout

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -4,11 +4,19 @@ int main()
     int v1,v2;
 
     scanf("%d %d", &v1, &v2);
+
     int a = v1, b = v2;
-    a = a + b; // a = a + b                      (nouvelle valeur de a)
-    b = a - b; // b = a - b => b = a + b - b = a (nouvelle valeur de b)
-    a = a - b; // a = a - b => a = a + b - a = b (nouvelle valeur de a)
+
+    a = a + b; 
+    // a = a + b                      (nouvelle valeur de a)
+
+    b = a - b; 
+    // b = a - b => b = a + b - b = a (nouvelle valeur de b)
+
+    a = a - b; 
+    // a = a - b => a = a + b - a = b (nouvelle valeur de a)
+
     
-    printf("Après échange: a = %d, b = %d", a, b);
+    printf("\nAprès échange: a = %d, b = %d", a, b);
 
 }
