eeb238a0fb29f0c24f4d6c0f8ca886d6518a92ff
Epiphane Nicoue Omar <omarepiphanefarouk@gmail.com>
2025-05-22 13:16:55 +0200

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -4,8 +4,8 @@ int main()
     int n;
     scanf("%d",&n);
     if (n%2==0)
-     printf("%d est pair\n", n);
+     printf("%d est pair.\n", n);
      else
-     printf("%d est impair\n", n);
+     printf("%d est impair.\n", n);
     
 }
