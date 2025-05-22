665f275bae7fead6c451a5a91df0b3fc37be0114
Epiphane Nicoue Omar <omarepiphanefarouk@gmail.com>
2025-05-22 13:56:31 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -3,9 +3,9 @@ int main()
 {
     int a,b;
     scanf("%d %d", &a, &b);
-    nb1 = nb1 + nb2; // a = a + b                      (nouvelle valeur de a)
-    nb2 = nb1 - nb2; // b = a - b => b = a + b - b = a (nouvelle valeur de b)
-    nb1 = nb1 - nb2; // a = a - b => a = a + b - a = b (nouvelle valeur de a)
+    a = a + b; // a = a + b                      (nouvelle valeur de a)
+    b = a - b; // b = a - b => b = a + b - b = a (nouvelle valeur de b)
+    a = a - b; // a = a - b => a = a + b - a = b (nouvelle valeur de a)
     printf("\nAprès échange: a = %d, b = %d", nb1, nb2);
 
 }
