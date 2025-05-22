4167135e0247b1dc2a1c23376d7ecfcd253a6212
Epiphane Nicoue Omar <omarepiphanefarouk@gmail.com>
2025-05-22 14:02:54 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main()
-{
+{   // déclaration de variable 
     int n;
     scanf("%d",&n);
+    // vérifier si le nombre est divisible par deux 
+    // si oui, le nombre est impair, sinon impair
     if (n%2==0)
      printf("%d est pair.\n", n);
      else
