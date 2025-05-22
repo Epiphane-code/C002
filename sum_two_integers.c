02f8ffcf4a7920f4e7e1d354f45cfa0b7a76fec3
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-22 13:07:04 +0200

    test

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,8 +1,7 @@
-=#include <stdio.h>
+#include <stdio.h>
 int main()
 {
     int nb1, nb2, somme;
     scanf("%d %d", &nb1, &nb2);
-    somme = nb1 + nb2;
-    printf("La somme est:%d\n", somme);
+    printf("La somme est:%d\n", nb1 + nb2);
 }
